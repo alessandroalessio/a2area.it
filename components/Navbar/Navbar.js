e2b85a59d6a0bb252d9df232f489939c6557110c
Alessandro <info@a2area.it>
2022-09-07 14:05:06 +0200

    Fix some errors

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -8,14 +8,14 @@ function Navbar (){
     return(
         <>
         <div className="flex m-4">
-            <div className="w-2/12 md:w-1/12 flex items-center">
+            <div className="w-2/12 flex items-center">
                 <Link href="/" title={commonData.SiteName}>
                     <a className="block">
                         <Image src={Logo} alt="" width={46} height={41} />
                     </a>
                 </Link>
             </div>
-            <nav className="w-9/12 hidden: md:flex justify-center items-center">
+            <nav className="w-8/12 flex justify-center items-center">
                 <ul className="hidden md:flex">
                     <li className="my-3 mx-6"><Link href="/"><a className="hover-menu">Chi sono</a></Link></li>
                     <li className="my-3 mx-6"><Link href="/"><a className="hover-menu">Servizi</a></Link></li>
@@ -29,7 +29,7 @@ function Navbar (){
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 mr-0 md:mr-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                     </svg>
-                    <span className="hidden md:inline-block">Preventivo gratuito</span>
+                    <span className="hidden sm:tracking-tighter sm:text-xs md:inline-block">Preventivo gratuito</span>
                 </div>
                 <MenuOverlay />
             </div>
--- pages/index.js
+++ pages/index.js
@@ -70,16 +70,16 @@ export default function Home({services}) {
           </div>
 
           <div className="flex gap-3 my-4 text-left">
-            {services.map(service => {
+            {services.map( (service, iService) => {
 
             const {slug, frontmatter} = service
             const {title, icon, excerpt} = frontmatter
 
-              return <div className="w-4/12 bg-white/5 duration-500 ease-in-out cursor-pointer hover:bg-white/10 p-8 rounded-xl">
+              return <div key={iService} className="w-4/12 bg-white/5 duration-500 ease-in-out cursor-pointer hover:bg-white/10 p-8 rounded-xl">
                 <Link href={'/services/' + slug}>
                   <a>
                     <div className="icon-wrapper">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="icon">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
                       </svg>
                     </div>
