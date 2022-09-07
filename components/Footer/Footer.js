25a0a72b8a20cf35126e150e8eceeff116497fbd
Alessandro <info@a2area.it>
2022-09-07 09:00:34 +0200

    Completed Hero without animations

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,6 +3,7 @@ const commonData = require('../../data/common.json')
 function Footer(){
     return(
         <>
+        
             <div className="fixed w-full bottom-0 left-0 text-center p-4 text-xs bg-dark">
                 P.IVA {commonData.VATNumber}
             </div>
--- pages/index.js
+++ pages/index.js
@@ -20,16 +20,30 @@ export default function Home() {
       <main>
         <Navbar />
 
-        <div className="hero pt-24 pb-36">
-          <div className="max-w-6xl mx-auto flex items-center h-1/2">
-            <div className="w-4/12">
-              <Image src={Me} title="Alessandro Alessio" alt="Alessandro Alessio" />
+        <div className="hero pt-0 md:pt-24 pb-36">
+          <div className="max-w-6xl mx-auto md:flex items-center h-1/2">
+            <div className="w-full md:w-4/12 pt-2 px-12 md:p-0">
+              <Image src={Me} title="Alessandro Alessio" alt="Alessandro Alessio" className="w-1/4 md:w-full" />
             </div>
-            <div className="w-8/12">
-              <h2 className="text-5xl p-8">Sono Alessandro.<br /> Realizzo <strong>Siti Web e Software</strong><br /> per privati e aziende.<br /> Se vuoi <a title="Preventivo Realizzazione Sito Web Alessandria"><strong>contattami</strong></a>!</h2>
+            <div className="w-full md:w-8/12">
+              <h2 className="text-4xl md:text-2xl lg:text-5xl p-8 md:p-8">
+                Sono Alessandro.<br />
+                Realizzo <strong>Siti Web e Software</strong> per privati e aziende.<br />
+                Se vuoi <Link href=""><a title="Preventivo Realizzazione Sito Web Alessandria">contattami</a></Link>!</h2>
             </div>
           </div>
         </div>
+
+        <section className="px-32 mb-8 text-center">
+          <h2 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-br from-emerald-500 to-purple-500">Servizi</h2>
+        </section>
+        <section className="px-32 mb-8 text-center">
+          <h2 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-br from-emerald-500 to-purple-500">Lavori</h2>
+        </section>
+        <section className="px-32 mb-8 text-center">
+          <h3 className="text-md uppercase tracking-widest">Siti Web Alessandria</h3>
+          <h2 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-br from-emerald-500 to-purple-500">Tecnologia dei siti web</h2>
+        </section>
 
         <Footer />
       </main>
